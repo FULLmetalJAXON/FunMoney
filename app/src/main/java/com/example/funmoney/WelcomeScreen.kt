1a8143e05aad36db4b4ea5fcd56830763bb6c613
FULLmetalJAXON <78572667+FULLmetalJAXON@users.noreply.github.com>
2021-03-24 12:39:22 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/funmoney/WelcomeScreen.kt
@@ -0,0 +1,20 @@
+package com.example.funmoney
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
